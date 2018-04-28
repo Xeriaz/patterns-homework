4da869e06137f0e32146f720e1de29c6bc0b1a44
Airidas Amšiejus <airidasams@gmail.com>
2018-04-28 17:35:16 +0300

    Configuration rootNode

--- src/WeatherBundle/DependencyInjection/Configuration.php
+++ src/WeatherBundle/DependencyInjection/Configuration.php
@@ -4,6 +4,9 @@ namespace Nfq\WeatherBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class Configuration implements ConfigurationInterface
 {
@@ -11,6 +14,35 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('nfq_weather');
+
+        $rootNode
+            ->children()
+                ->scalarNode('provider')->end()
+            ->end()
+            ->children()
+                ->arrayNode('providers')
+                    ->children()
+                        ->arrayNode('openweathermap')
+                            ->children()
+                                ->scalarNode('api_key')->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                    ->children()
+                        ->arrayNode('delegating')
+                            ->children()
+                                ->arrayNode('providers')
+                                    ->children()
+                                        ->scalarNode('0')->end()
+                                        ->scalarNode('1')->end()
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
